9e7dc333919a2ea6b13ef6ba165c66c256758a8f
Olivier <olivier.pire@outlook.fr>
2022-06-14 17:40:11 +0200

    HashRouter remplace BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import CreateEmployee from "./Pages/CreateEmployee";
 import CurrentEmployees from "./Pages/CurrentEmployees"
 import NotFound from "./Pages/NotFound";
 
 const App = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" exact element={<CreateEmployee />} />
         <Route path="/*" exact element={<NotFound />} />
         <Route path="/current-employees" exact element={<CurrentEmployees />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
